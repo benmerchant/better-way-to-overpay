460f6dcca07af012a0096cc17b27bdbf0a831bce
Ben Merchant <Ben.Merchant@uspsector.com>
2020-04-25 09:45:18 -0400

    connec to Mongo

---
+++ MongoDB/documents/users.ts
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    first_name: "Ben",
+    last_name: "Merchant",
+    street: "",
+    city: "",
+    state: "",
+    zip: "",
+    phone: "",
+    email: "",
+    password: "",
+    date_of_birth: "",
+    paymentInstruments: {
+      type: "",
+      nickname: ""
+    }
+  }
+]
--- MongoDB/init.ts
+++ MongoDB/init.ts
@@ -1,6 +1,31 @@
-const db = 'opay'
-const dbUser = ''
-const dbPassword = ''
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
 
+import USERS_DOCUMENT from './documents/users.ts'
 
-const dbConnection = `mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]`
+const dbName = 'opay'
+// TODO: cant auth in
+// const dbUser = 'benjo'
+// const dbPassword = 'changeToYourPassword'
+const port = 27017
+const host = '127.0.0.1'
+const uri = `mongodb://${host}:${port}/${dbName}`
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+const client = new MongoClient(uri, options)
+
+client.connect(err => {
+  assert.equal(null,err)
+  console.log('connected to MongoDB...')
+
+  const db = client.db(dbName)
+
+  db.collection('users').insertOne(USERS_DOCUMENT[0], (err, res) => {
+    assert.equal(null,err)
+    console.log(`inserted 1 document`);
+    client.close()
+  })
+
+  
+  
+})
