a7197da0e5a14d54c95a070cd66565b651bb7b52
Ben Merchant <Ben.Merchant@uspsector.com>
2020-04-30 21:33:54 -0400

    set up the most basic of GET routes

--- ExpressAPI/server.ts
+++ ExpressAPI/server.ts
@@ -12,6 +12,22 @@ app.get('/', (_req: any, res: any) => {
   res.send('Heyooooo, World!')
 })
 
+app.get('/automobiles', (_req: any, res: any) => {
+  res.send('Heyooooo, Automobiles!')
+})
+
+app.get('/contracts', (_req: any, res: any) => {
+  res.send('Heyooooo, Contracts!')
+})
+
+app.get('/creditCardSystems', (_req: any, res: any) => {
+  res.send('Heyooooo, Credit Card Systems!')
+})
+
+app.get('/users', (_req: any, res: any) => {
+  res.send('Heyooooo, Users!')
+})
+
 app.listen(port, () => {
   console.log(`oPay API listening on ${host}${port}`)
   
