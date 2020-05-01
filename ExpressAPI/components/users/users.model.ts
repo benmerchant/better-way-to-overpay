6e630708be26977efd4450b23df686fdfffcfe07
Ben Merchant <Ben.Merchant@uspsector.com>
2020-05-01 13:17:15 -0400

    create mongoose model

---
+++ ExpressAPI/components/users/users.model.ts
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const UserSchema = mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  street: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  zip: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date_of_birth: {
+    type: String,
+    required: Date
+  },
+  paymentInstruments: [{
+    type: {
+      type: String,
+      required: true
+    },
+    nickname: {
+      type: String,
+      required: true
+    }
+  }]
+})
+
+module.exports = mongoose.model('User', UserSchema)
