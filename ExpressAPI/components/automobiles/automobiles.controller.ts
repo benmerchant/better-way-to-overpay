544ad6e7ad193b5018ed67f034ba9518b0a05613
Ben Merchant <Ben.Merchant@uspsector.com>
2020-05-02 16:31:50 -0400

    fix tsc dist file

--- ExpressAPI/components/automobiles/automobiles.controller.ts
+++ ExpressAPI/components/automobiles/automobiles.controller.ts
@@ -2,7 +2,7 @@ import Automobile from './automobiles.model'
 
 const AutomobileController = {
   // GET all automobiles
-  getAutomobiles: (req, res, next) => {
+  getAutomobiles: (req: any, res: any, next: any) => {
     Automobile.find({}).exec((err, automobiles) => {
       if (err) res.status(500).json({ error: err })
       return res.status(200).json({ automobiles: automobiles })
--- ExpressAPI/components/creditCardSystems/creditCardSystems.controller.ts
+++ ExpressAPI/components/creditCardSystems/creditCardSystems.controller.ts
@@ -2,7 +2,7 @@ import CreditCardSystem from './creditCardSystems.model'
 
 const CreditCardSystemController = {
   // GET all creditCardSystems
-  getCreditCardSystems: (req, res, next) => {
+  getCreditCardSystems: (req:any, res:any, next:any) => {
     CreditCardSystem.find({}).exec((err, creditCardSystems) => {
       if (err) res.status(500).json({ error: err })
       return res.status(200).json({ creditCardSystems: creditCardSystems })
