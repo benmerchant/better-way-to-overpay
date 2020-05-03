e3cc7bbc5b908e2f16f15e6f7946ed494a5eb060
Ben Merchant <Ben.Merchant@uspsector.com>
2020-05-03 16:17:37 -0400

    allow console warn/error/info refactor imports tell TS the resolve modules with node flag

--- MongoDB/src/init.ts
+++ MongoDB/src/init.ts
@@ -1,82 +1,78 @@
-const MongoClient = require('mongodb').MongoClient // TODO: obvs, fix these module imports
-import assert from 'assert'
-const dotenv = require('dotenv')
+// TODO: obvs, fix these module imports
+import assert from 'assert';
 
-dotenv.config()
 
 import USERS_DOCUMENT from './documents/users'
 import CONTRACTS_DOCUMENT from './documents/contracts'
 import AUTOMOBILES_DOCUMENT from './documents/automobiles'
 import CREDIT_CARD_SYSTEMS_DOCUMENT from './documents/creditCardSystems'
 
-const dbName = 'opay'
+const MongoDB = require('mongodb');
+
+const { MongoClient } = MongoDB;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const dbName = 'opay';
 // TODO: cant auth in
 // const dbUser = 'benjo'
 // const dbPassword = 'changeToYourPassword'
-const port = process.env.PORT
-const dbUrl = process.env.DB_BASE_URL
-const uri = `${dbUrl}:${port}/${dbName}`
+const port = process.env.PORT;
+const dbUrl = process.env.DB_BASE_URL;
+const uri = `${dbUrl}:${port}/${dbName}`;
 
-const options = { useNewUrlParser: true, useUnifiedTopology: true }
-const client = new MongoClient(uri, options)
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const client = new MongoClient(uri, options);
 
 client.connect((err: object) => {
-  assert.equal(null,err)
-  const seedPromises : Promise<any>[] = []
-  console.log('You have successfully connected to MongoDB...\n')
-  const db = client.db(dbName)
+  assert.equal(null, err);
+  const seedPromises : Promise<any>[] = [];
+  console.info('You have successfully connected to MongoDB...\n');
+  const db = client.db(dbName);
 
   seedPromises.push(new Promise((resolve, reject) => {
-    const collectionName = 'users'
+    const collectionName = 'users';
     db.collection(collectionName) // TODO: res needs type
       .insertMany(USERS_DOCUMENT, (err: object, res: any) => {
-        if (err) reject(err)
-        else resolve({ mongoResult: res.result, collectionName: collectionName })
-      })
-    })
-  )
+        if (err) reject(err);
+        else resolve({ mongoResult: res.result, collectionName });
+      });
+  }));
 
   seedPromises.push(new Promise((resolve, reject) => {
-    const collectionName = 'contracts'
+    const collectionName = 'contracts';
     db.collection(collectionName) // TODO: res needs type
-      .insertMany(CONTRACTS_DOCUMENT, (err: object, res: any) => { 
-        if (err) reject(err)
-        else resolve({ mongoResult: res['result'], collectionName: collectionName })
-      })
-    })
-  )
+      .insertMany(CONTRACTS_DOCUMENT, (err: object, res: any) => {
+        if (err) reject(err);
+        else resolve({ mongoResult: res.result, collectionName });
+      });
+  }));
 
   seedPromises.push(new Promise((resolve, reject) => {
-    const collectionName = 'creditCardSystems'
+    const collectionName = 'creditCardSystems';
     db.collection(collectionName) // TODO: res needs type
-      .insertMany(CREDIT_CARD_SYSTEMS_DOCUMENT, (err: object, res: any) => { 
-        if (err) reject(err)
-        else resolve({ mongoResult: res['result'], collectionName: collectionName })
-      })
-    })
-  )
+      .insertMany(CREDIT_CARD_SYSTEMS_DOCUMENT, (err: object, res: any) => {
+        if (err) reject(err);
+        else resolve({ mongoResult: res.result, collectionName });
+      });
+  }));
 
   seedPromises.push(new Promise((resolve, reject) => {
-    const collectionName = 'automobiles'
+    const collectionName = 'automobiles';
     db.collection(collectionName) // TODO: res needs type
-      .insertMany(AUTOMOBILES_DOCUMENT, (err: object, res: any) => { 
-        if (err) reject(err)
-        else resolve({ mongoResult: res['result'], collectionName: collectionName })
-      })
-    })
-  )
-  
+      .insertMany(AUTOMOBILES_DOCUMENT, (err: object, res: any) => {
+        if (err) reject(err);
+        else resolve({ mongoResult: res.result, collectionName });
+      });
+  }));
+
   // TODO: handle errors
   Promise.all(seedPromises)
-    .then(messages => {
-      messages.forEach(message => {
+    .then((messages) => {
+      messages.forEach((message) => {
         // TODO: implement dynamic plural
-        console.log(`${message.mongoResult.n} ${message.collectionName} documents successfully inserted...`)        
-      })
-    })
-})
-
-
-
-
-
+        console.info(`${message.mongoResult.n} ${message.collectionName} documents successfully inserted...`);
+      });
+    });
+});
--- MongoDB/src/wipeout.ts
+++ MongoDB/src/wipeout.ts
@@ -1,45 +1,45 @@
-const MongoClient = require('mongodb').MongoClient
-import assert from 'assert'
-const dotenv = require('dotenv')
+import assert from 'assert';
 
-dotenv.config()
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const COLLECTIONS = [
   'users',
   'contracts',
   'creditCardSystems',
-  'automobiles'
-]
+  'automobiles',
+];
 
-const dbName = 'opay'
-const port = process.env.PORT
-const dbUrl = process.env.DB_BASE_URL
-const uri = `${dbUrl}:${port}/${dbName}`
+const dbName = 'opay';
+const port = process.env.PORT;
+const dbUrl = process.env.DB_BASE_URL;
+const uri = `${dbUrl}:${port}/${dbName}`;
 
-const options = { useNewUrlParser: true, useUnifiedTopology: true }
-const client = new MongoClient(uri, options)
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const client = new MongoClient(uri, options);
 
 client.connect((err: object) => {
-  assert.equal(null, err)
-  const deletePromises: Promise<any>[] = []
-  console.log('You have successfully connected to MongoDB...\n')
-  const db = client.db(dbName)
+  assert.equal(null, err);
+  const deletePromises: Promise<any>[] = [];
+  console.info('You have successfully connected to MongoDB...\n');
+  const db = client.db(dbName);
 
-  for (let ii=0; ii<COLLECTIONS.length; ii++) {
+  for (let ii = 0; ii < COLLECTIONS.length; ii + 1) {
     deletePromises.push(new Promise((resolve, reject) => {
       db.collection(COLLECTIONS[ii])
         .deleteMany({}, (err: object, res: any) => {
-          if (err) reject(err) // TODO: res needs type
-          else resolve({ mongoResult: res['result'], collectionName: COLLECTIONS[ii] })
-        })
-    })
-    )
+          if (err) reject(err); // TODO: res needs type
+          else resolve({ mongoResult: res.result, collectionName: COLLECTIONS[ii] });
+        });
+    }));
   }
 
   Promise.all(deletePromises)
-    .then(messages => {
-      messages.forEach(message => {
-        console.log(`${message.mongoResult.n} ${message.collectionName} documents successfully deleted...`)
-      })
-    })
-})
+    .then((messages) => {
+      messages.forEach((message) => {
+        console.info(`${message.mongoResult.n} ${message.collectionName} documents successfully deleted...`);
+      });
+    });
+});
